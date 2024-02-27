2dde5f5bbef03e35656660990c312d5c82083013
dongliang <dongliangfe@gmail.com>
2024-02-27 13:05:19 +0800

    feat: 增加【验证回文串】题解

--- 125.验证回文串.js
+++ 125.验证回文串.js
@@ -9,8 +9,31 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
+var isPalindrome = function (s) {
+  let left = 0, right = s.length - 1;
 
+  while (left < right) {
+    if (!isValidateChar(s[left])) {
+      left++;
+      continue;
+    }
+
+    if (!isValidateChar(s[right])) {
+      right--;
+      continue;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
+
+function isValidateChar(c) {
+  return /[a-zA-Z0-9]/.test(c);
+}
 // @lc code=end
 
