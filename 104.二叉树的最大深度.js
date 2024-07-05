f9eb09ab4ef6d063a0ce6506ca842d2f4f47a949
dongliang <dongliangfe@gmail.com>
2024-07-05 10:03:53 +0800

    update: 更新 [104] 二叉树的最大深度 题解

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+// 递归的DFS实现
+// 递归地计算每个子树的最大深度，并取左右子树深度的最大值加1。
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+};
+
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  const queue = [root];
+  let depth = 0;
+
+  // 按层遍历，麻烦的地方是每一层遍历结束都要给结果 +1
+  // 也就是需要判断怎么样才是一层结束
+  while (queue.length) {
+    // 每一层的节点数量
+    const levelSize = queue.length
+    // 一次要把一层的节点全部遍历结束
+    for (let i = 0; i < levelSize; i++) {
+      const current = queue.shift()
+      if (current && current.left !== null) {
+        queue.push(current.left)
+      }
+
+      if (current && current.right !== null) {
+        queue.push(current.right)
+      }
+    }
+  }
+
+  return depth
+};
+// @lc code=end
+
