dd70dd6e21648a282ff1756f12528b38a1f51cfd
dongliang <dongliangfe@gmail.com>
2024-07-05 10:22:29 +0800

    update: 更新 [344.反转字符串] 递归版本题解

--- 344.反转字符串.js
+++ 344.反转字符串.js
@@ -19,5 +19,27 @@ var reverseString = function (s) {
     right--
   }
 };
+
+// 递归版本
+var reverseString = function (s) {
+  const left = 0
+  const right = s.length - 1
+
+  const helper = (left, right) => {
+    // 递归终止条件：当左右指针相遇或交错时
+    if (left >= right) {
+      return
+    }
+
+    // 交换左右指针所指的元素
+    [s[left], s[right]] = [s[right], s[left]]
+    // 递归处理剩下的元素
+    helper(left + 1, right - 1)
+  }
+
+  helper(left, right)
+
+  return s
+};
 // @lc code=end
 
