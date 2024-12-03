58b324646671db7bca445a6fdd9767e9bfd35b1c
dongliang <dongliangfe@gmail.com>
2024-12-03 13:41:29 +0800

    feat: 更新 [509] 斐波那契数题解

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -20,5 +20,40 @@ var fib = function (n) {
 
   return fib(n - 1) + fib(n - 2)
 };
+
+
+var fib = function (n) {
+  const hashMap = new Map([
+    [0, 0],
+    [1, 1]
+  ])
+
+  function helper(n) {
+    if (hashMap.has(n)) {
+      return hashMap.get(n)
+    }
+
+    const result = helper(n - 1) + helper(n - 2)
+    hashMap.set(n, result)
+
+    return result
+  }
+
+  return helper(n)
+}
+
+var fib = function (n) {
+  if (n <= 1) {
+    return n
+  }
+
+  const result = [0, 1]
+
+  for (let i = 2; i <= n; i++) {
+    result[i] = result[i - 1] + result[i - 2]
+  }
+
+  return result[n]
+}
 // @lc code=end
 
