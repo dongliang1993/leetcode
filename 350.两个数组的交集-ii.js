d4e5ab0c377c895eab86aa93c8c4c704b5e1c4f8
dongliang <dongliangfe@gmail.com>
2025-09-08 19:44:57 +0800

    feat: 优化【两个数组的交集 II】的实现，简化代码逻辑并提高可读性

--- 350.两个数组的交集-ii.js
+++ 350.两个数组的交集-ii.js
@@ -11,25 +11,24 @@
  * @return {number[]}
  */
 var intersect = function (nums1, nums2) {
-  const numMap = new Map()
+  const freqMap = new Map()
   const result = []
 
-  // 遍历 nums1 数组，记录每个元素出现的次数
-  for (let i = 0; i < nums1.length; i++) {
-    const sum = numMap.get(nums1[i]) || 0
-    numMap.set(nums1[i], sum + 1)
+  for (const num of nums1) {
+    const count = freqMap.get(num) || 0
+    freqMap.set(num, count + 1)
   }
 
-  // 遍历 nums2 数组，检查对应元素在 numMap 中的出现次数
-  for (let i = 0; i < nums2.length; i++) {
-    if (numMap.has(nums2[i]) && numMap.get(nums2[i]) > 0) {
-      result.push(nums2[i])
-      numMap.set(nums2[i], numMap.get(nums2[i]) - 1)
+
+  for (const num of nums2) {
+    if (freqMap.get(num) > 0) {
+      result.push(num)
+      freqMap.set(num, freqMap.get(num) - 1)
     }
   }
 
   return result
-};
+}
 // @lc code=end
 
 
